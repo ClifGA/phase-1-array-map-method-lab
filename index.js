955cf20f0d3c366b909ed55b345ad109aee99bc7
Clifton <clifga@gmail.com>
2023-03-21 20:37:22 -0400

    complete

--- index.js
+++ index.js
@@ -10,7 +10,10 @@ const tutorials = [
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
 ];
-
 const titleCased = () => {
-  return tutorials
+  return  tutorials.map(sentence => {
+  const words = sentence.split(" ");
+  const capitalizedWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1));
+  return capitalizedWords.join(" ");
+    })
 }
